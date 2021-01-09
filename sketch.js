81215d094e3b480bcd2b192c5c17988b0a046203
Shaurya-Srivastava08 <75517438+Shaurya-Srivastava08@users.noreply.github.com>
2021-01-09 12:33:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+// 1 maggi 
+// 2 first take out the maggi out off the packet.
+// 3 put water in the boiling pan.
+// 4 put maggi in the boiling pan.
+// 5 let get  maggi soaked.
+// 6 then when it get soaked put maggi masala in it.
+// 7 then you can eat it peacefully with suace or cold drink.
+
+// thank you
+
